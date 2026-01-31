810c8618f9cede995ae89ef65c99b45703e76094
papuganiharshitha04-oss <papuganiharshitha04@gmail.com>
2026-01-31 14:55:33 +0530

    Add files via upload

---
+++ demo2.java
@@ -0,0 +1,10 @@
+public class demo2
+{
+    public static void main(String args[])
+    {
+        System.out.println("hello world");
+        System.out.println("welcome to mother theresa");
+        System.out.println("good morning");
+
+    }
+}
